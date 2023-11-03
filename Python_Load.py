e4a39a5de48127725d7a271577809d719d91edab
Omar Shatrat <49295640+omarshatrat@users.noreply.github.com>
2023-11-03 16:45:36 -0500

    Update Python_Load.py

--- Python_Load.py
+++ Python_Load.py
@@ -2,14 +2,14 @@ import sqlite3
 import pandas as pd
 from sqlalchemy import create_engine
 
-csv_file = pd.read_csv('hw5_original.csv')
-db_file = 'hw5.db'
+csv_file = pd.read_csv('entertainment_original.csv')
+db_file = 'entertainment.db'
 
 
 conn = sqlite3.connect(db_file)
 
-engine = create_engine('sqlite:///hw5_python.db')
-csv_file.to_sql('hw5_python', con = engine, if_exists = 'replace', index = False)
+engine = create_engine('sqlite:///entertainment_python.db')
+csv_file.to_sql('entertainment_python', con = engine, if_exists = 'replace', index = False)
 
 
 conn.close()
